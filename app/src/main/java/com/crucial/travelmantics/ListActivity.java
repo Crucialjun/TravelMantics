4c04592395e29dbeb3f14495d18d193ebf4439b4
Nicholas <nich.otieno@gmail.com>
2019-07-25 22:40:25 +0300

    logout

--- app/src/main/java/com/crucial/travelmantics/ListActivity.java
+++ app/src/main/java/com/crucial/travelmantics/ListActivity.java
@@ -1,14 +1,21 @@
 package com.crucial.travelmantics;
 
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -17,6 +24,7 @@ import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity{
 
+    private static final String TAG = ListActivity.class.getSimpleName();
     ArrayList<TravelDeal> deals;
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseReference;
@@ -43,6 +51,18 @@ public class ListActivity extends AppCompatActivity{
             case R.id.insert_menu:
                 Intent intent = new Intent(ListActivity.this, DealActivity.class);
                 startActivity(intent);
+                return true;
+            case R.id.logout_menu:
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                // ...
+                                Log.d(TAG,"User Logged Out");
+                                FirebaseUtil.attachListener();
+                            }
+                        });
+                FirebaseUtil.detachListener();
                 return true;
         }
         return super.onOptionsItemSelected(item);
